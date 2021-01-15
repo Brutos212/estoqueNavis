5afe195ef3da2b49cd95596c86fa78decbf2d8fa
estudante <estudante@NavaEstudos.estudo>
2021-01-15 17:00:52 -0300

    TabularInline. Close #7

--- projetonavis/estoque/admin.py
+++ projetonavis/estoque/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 
 from .models import Estoque, EstoqueItens
 
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+
 @admin.register(Estoque)
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInline,)
     list_display = ('__str__', 'nf')
     search_fields = ('nf',)
-    search_fields = ('Service Tag',)
+    #search_fields = ('Service Tag',)
     list_fields = ('funcionario',)
     date_hierarchy = 'created'
