c0a38400fc79f75987f206d9cb4a75a9e98abc9e
estudante <estudante@NavaEstudos.estudo>
2021-01-26 15:26:16 -0300

    Criando dados para o produto. Close #8

--- create_data.py
+++ create_data.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projetonavis.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projetosnavis.settings")
 django.setup()
 
 import string
@@ -9,11 +9,14 @@ import timeit
 from random import choice, random, randint
 from projetonavis.produtos.models import Produtos
 
+
 class Utils:
     """Métodos genéricos."""
+
     @staticmethod
     def gen_digits(max_length):
         return str(''.join(choice(string.digits) for i in range(max_length)))
+
 
 class ProdutosClass:
     @staticmethod
@@ -27,18 +30,18 @@ class ProdutosClass:
                 ncm=Utils.gen_digits(8),
                 preco=random() * randint(10, 50),
                 estoque=randint(10, 200),
+
             )
-            
             obj = Produtos(**data)
             aux.append(obj)
         Produtos.objects.bulk_create(aux)
 
 
 produtos = (
-    "Roupas"
-    "Tenis"
-    "chinelos"
-
+    'Amora',
+    'Azul',
+    'Adeus'
+    'Ateste'
 )
 
 tic = timeit.default_timer()
@@ -46,4 +49,4 @@ ProdutosClass.criar_produtos(produtos)
 
 toc = timeit.default_timer()
 
-print("Tempo: ", toc - tic)
+print('Tempo', tic - toc)
